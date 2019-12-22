57e3d2a9b0c8d11e59a80fc0200c3eb610899e65
iAxick_notebook <directxinbox@gmail.com>
2019-12-22 18:09:43 +0200

    i have started to create react app

---
+++ react-livello/src/components/MainScreen.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+export default function () {
+    return(
+        <section id="mainScrImg">
+            <nav id="mainMenu">
+                <span><a href="#"><i className="fas fa-mountain"></i></a></span>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                </ul>
+            </nav>
+            <header>
+                <h1>Livello</h1>
+                <span>Our clothing - your comfort</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias blanditiis dicta
+                    doloribus error eveniet incidunt ipsa magnam officiis praesentium, provident qui quisquam, rerum
+                    saepe soluta, suscipit unde veritatis voluptates?</p>
+            </header>
+
+            <span className="mainArrow">
+        </span>
+        </section>
+    )
+}
